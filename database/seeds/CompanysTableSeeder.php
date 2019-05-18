77fe50c6bca181153a4a920c0fac060c28dda311
hiroki <jazzmusixbass.sm.p@gmail.com>
2019-05-18 15:07:12 +0900

    会社テーブルのマイグレーション、シーディング

---
+++ database/seeds/CompanysTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CompanysTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $names = [
+            'carecon',
+            'uptech',
+            'cyber',
+        ];
+
+        foreach ($names as $name) {
+            DB::table('companys')->insert([
+                'name' => $name,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
