2f13904fdabf6d84c49b9277d5885cf4db0a67f8
hiroki <jazzmusixbass.sm.p@gmail.com>
2019-05-18 03:25:55 +0000

    subskillテーブル、モデル、シーダー作成

---
+++ database/migrations/2019_05_16_224424_create_subskills_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubskillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subskills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('skill_id')->unsigned();
+            $table->string('title',20);
+            $table->integer('evaluation')->default(1);
+            $table->timestamps();
+
+            $table->foreign('skill_id')->references('id')->on('skills');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subskills');
+    }
+}
---
+++ database/seeds/SubskillsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SubskillsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $titles = [
+            'クライアント折衝',
+            'WBS調整',
+            'リリース手順書',
+            'チームマネジメント',
+            '資料作成',
+        ];
+        foreach ($titles as $title) {
+            DB::table('subskills')->insert([
+                'skill_id' => 1,
+                'title' => $title,
+                'evaluation' => mt_rand(1,5),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
