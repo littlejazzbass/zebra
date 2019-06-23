0cee6657773433479baf9a7e194bc54bc8319c23
hiroki <jazzmusixbass.sm.p@gmail.com>
2019-06-23 22:37:10 +0900

    サービスクラスの設定してみた

---
+++ app/Services/Service.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Services;
+
+use Illuminate\Support\Facades\App;
+
+class Services{
+    public function __construct(){
+        
+    }
+}
---
+++ app/Services/UserService.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Services;
+
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserService extends Services
+{
+    public function __construct()
+    {
+
+    }
+    /** #01
+    *  DBからユーザー情報を取得
+    *  @var int $id ユーザーid
+    */
+    public function getUser(int $id)
+    {
+        $user = User::query()
+        ->select([
+            'users.id as user_id',
+            'users.name as user_name',
+            'users.email',
+            'users.admin_flg as user_status',
+        ])
+        ->where('users.id',$id)
+        ->first();
+
+        return $user;
+    }
+}
